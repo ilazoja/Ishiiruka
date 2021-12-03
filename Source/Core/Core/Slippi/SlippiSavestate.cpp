1e34ccaa0dd3c6cee7b19ea525d1228f6a6215fe
Jared White <jwhite2292forward@gmail.com>
2021-12-03 16:59:49 -0600

    Update backup regions to the player, item, stage, and physics instances locations in Brawl's memory.

--- Source/Core/Core/Slippi/SlippiSavestate.cpp
+++ Source/Core/Core/Slippi/SlippiSavestate.cpp
@@ -50,53 +50,16 @@ bool cmpFn(SlippiSavestate::PreserveBlock pb1, SlippiSavestate::PreserveBlock pb
 void SlippiSavestate::initBackupLocs()
 {
 	static std::vector<ssBackupLoc> fullBackupRegions = {
-	    {0x80005520, 0x80005940, nullptr}, // Data Sections 0 and 1
-	    {0x803b7240, 0x804DEC00, nullptr}, // Data Sections 2-7 and in between sections including BSS
-
-	    // Full Unknown Region: [804fec00 - 80BD5C40)
-	    // https://docs.google.com/spreadsheets/d/16ccNK_qGrtPfx4U25w7OWIDMZ-NxN1WNBmyQhaDxnEg/edit?usp=sharing
-	    {0x8065c000, 0x8071b000, nullptr}, // Unknown Region Pt1. Maybe get the low bound pointer at 804d5c10 and the size of the audio heap at 804d5e18
-	    {0x80bd5c40, 0x811AD5A0, nullptr}, // Unknown Region Pt2, Heap [80bd5c40 - 811AD5A0). Gets overwritten on init
+	    {0x8123ab60, 0x8128cb60, nullptr}, // Fighter 1 instance
+	    {0x8128cb60, 0x812deb60, nullptr}, // Fighter 2 instance
+	    {0x812deb60, 0x81330b60, nullptr}, // Fighter 3 instance
+	    {0x81330b60, 0x81382b60, nullptr}, // Fighter 4 instance
+	    {0x81382b60, 0x814ce460, nullptr}, // Item instance
+	    {0x814ce460, 0x8154e560, nullptr}, // Stage instance
+	    {0x8154e560, 0x81601960, nullptr}  // Physics instance
 	};
 
-	static std::vector<PreserveBlock> excludeSections = {
-	    // Sound stuff
-	    {0x804031A0, 0x24},    // [804031A0 - 804031C4)
-	    {0x80407FB4, 0x34C},   // [80407FB4 - 80408300)
-	    {0x80433C64, 0x1EE80}, // [80433C64 - 80452AE4)
-	    {0x804A8D78, 0x17A68}, // [804A8D78 - 804C07E0)
-	    {0x804C28E0, 0x399C},  // [804C28E0 - 804C627C)
-	    {0x804D7474, 0x8},     // [804D7474 - 804D747C)
-	    {0x804D74F0, 0x50},    // [804D74F0 - 804D7540)
-	    {0x804D7548, 0x4},     // [804D7548 - 804D754C)
-	    {0x804D7558, 0x24},    // [804D7558 - 804D757C)
-	    {0x804D7580, 0xC},     // [804D7580 - 804D758C)
-	    {0x804D759C, 0x4},     // [804D759C - 804D75A0)
-	    {0x804D7720, 0x4},     // [804D7720 - 804D7724)
-	    {0x804D7744, 0x4},     // [804D7744 - 804D7748)
-	    {0x804D774C, 0x8},     // [804D774C - 804D7754)
-	    {0x804D7758, 0x8},     // [804D7758 - 804D7760)
-	    {0x804D7788, 0x10},    // [804D7788 - 804D7798)
-	    {0x804D77C8, 0x4},     // [804D77C8 - 804D77CC)
-	    {0x804D77D0, 0x4},     // [804D77D0 - 804D77D4)
-	    {0x804D77E0, 0x4},     // [804D77E0 - 804D77E4)
-	    {0x804DE358, 0x80},    // [804DE358 - 804DE3D8)
-	    {0x804DE800, 0x70},    // [804DE800 - 804DE870)
-
-	    // The following need to be added to the ranges proper
-	    {0x804d6030, 0x4},  // ???
-	    {0x804d603c, 0x4},  // ???
-	    {0x804d7218, 0x4},  // ???
-	    {0x804d7228, 0x8},  // ???
-	    {0x804d7740, 0x4},  // ???
-	    {0x804d7754, 0x4},  // ???
-	    {0x804d77bc, 0x4},  // ???
-	    {0x804de7f0, 0x10}, // ???
-
-	    // Camera Blocks, Temporarily added here
-	    //{0x80452c7c, 0x2B0}, // Cam Block 1, including gaps
-	    //{0x806e516c, 0xA8},  // Cam Block 2, including gaps
-	};
+	static std::vector<PreserveBlock> excludeSections = {};
 
 	static std::vector<ssBackupLoc> processedLocs = {};
 
@@ -108,12 +71,6 @@ void SlippiSavestate::initBackupLocs()
 	}
 
 	shouldForceInit = false;
-
-	// Get Main Heap Boundaries
-	fullBackupRegions[3].startAddress = PowerPC::HostRead_U32(0x804d76b8);
-	fullBackupRegions[3].endAddress = PowerPC::HostRead_U32(0x804d76bc);
-	WARN_LOG(SLIPPI_ONLINE, "Heap start is: 0x%X", fullBackupRegions[3].startAddress);
-	WARN_LOG(SLIPPI_ONLINE, "Heap end is: 0x%X", fullBackupRegions[3].endAddress);
 
 	// Sort exclude sections
 	std::sort(excludeSections.begin(), excludeSections.end(), cmpFn);
